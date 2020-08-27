e0f45d0952eac92dfd39e32f587249e92a53b2b9
Calcium <61847384+Calcium02@users.noreply.github.com>
2020-08-27 10:00:35 -0400

    Delete VMLogin.bat

--- VMLogin.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set /p LoginPath=Which User?
-set /p VMPath=Which VM?
-set /p ProjectPath=Which Project?
-
-start C:\windows\system32\cmd.exe /k ""C:\Users\dakot\AppData\Local\Google\Cloud SDK\cloud_env.bat" && gcloud compute ssh %LoginPath%@%VMPath% --project %ProjectPath%" 
