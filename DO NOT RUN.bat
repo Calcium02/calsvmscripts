39cc3d622eb664af3547c193fa47a267d79f3bda
Calcium <dakotadanielsga@gmail.com>
2020-10-27 12:23:57 -0400

    Update DO NOT RUN.bat

--- DO NOT RUN.bat
+++ DO NOT RUN.bat
@@ -1,4 +1,6 @@
 @echo off
+echo This will harm your computer if you ran this file with admin privs. You have 30 seconds to exit out of this prompt. Upon exiting, please delete this file. This file is only for testing.
+timeout /t 30 /nobreak
 
 :: Get ADMIN Privs
 :-------------------------------------
